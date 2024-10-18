ab5892bd059c9cf060874a29c86d66918bfee4f8
Lee Stacey <155361420+LStace@users.noreply.github.com>
2024-10-18 13:11:22 +0100

    ball moves in random direction when instantiated

--- ball_escape/scripts/ball.gd
+++ ball_escape/scripts/ball.gd
@@ -10,7 +10,7 @@ var collide_cooldown = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	movement = Vector2.RIGHT.rotated(deg_to_rad(randf_range(0, 360)))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
